16273e907a17d4b1c7f7a963a9393eeaa756c16b
David <davidkueng@gmail.com>
2018-11-24 21:18:34 +0100

    moved folders

---
+++ quotes.js
@@ -0,0 +1,31 @@
+
+$(document).ready( () => { 
+
+let quotesURL = "http://quotes.stormconsultancy.co.uk/random.json";   
+
+let getQuote = () => {
+                    $.getJSON(quotesURL, (json) => {
+                        $("#text").html(JSON.stringify = `${json.quote}`);
+                        $("#author").html(JSON.stringify = "- " + `${json.author}`);
+                        });
+                    }
+
+let clickToTweet = () => { 
+                    let quoteContent = $("#text").html(); 
+                    window.open("http://twitter.com/intent/tweet?url=&text=" 
+                    + quoteContent); 
+                    };  
+
+
+    getQuote();
+        //tweet quote
+        $("#tweet-quote").on("click", clickToTweet) 
+       
+        //show next quote                        
+        $("#new-quote").on("click", getQuote) 
+                      
+})       
+
+
+
+
